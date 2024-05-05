3ea1638bb60ffe4c31449892ef6a6a8a732fb654
DESVO <57235312+DesvoSoft@users.noreply.github.com>
2024-05-04 20:58:57 -0600

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,9 @@
 import profilePic from './assets/images/ppmod.jpg'
-import Pic from './assets/images/PP.png'
 
 function Card() {
     return(
         <div className="card">
-            <img className="card-image" src={Pic} alt="profile picture"></img>
+            <img className="card-image" src={profilePic} alt="profile picture"></img>
             <h2 className='card-title'>Wan!</h2>
             <p className='card-text'>XD</p>
         </div>
